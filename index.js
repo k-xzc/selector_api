4026fa9998312db4b14f0576472bc248122364c8
root <root@ip-172-31-22-5.ap-southeast-1.compute.internal>
2019-06-19 06:08:41 +0000

    create api

---
+++ index.js
@@ -0,0 +1,26 @@
+var bodyParser = require('body-parser');
+var dateFormat = require('dateformat');
+var rp = require('request-promise');
+var cc_api_host = process.env.cc_api_host
+var cc_api_port = process.env.cc_api_port
+var cron = require('node-cron');
+
+cron.schedule('*/10 * * * * *', function(){
+   var json = JSON.parse('{"focus":["BTC","ETH"]}')
+   console.log(json)
+   postToCC(json)
+   console.log("done")
+})
+
+function postToCC(json) {
+  url = "http://" + cc_api_host + ":" + cc_api_port + "/getcurrency"
+  return rp({
+        uri: url,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: json,
+        json: true
+    })
+}
