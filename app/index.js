da4764340a250f0765359548810a2e1206294a27
kantsuw <kantsuwan@gmail.com>
2019-06-21 00:25:49 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ var cc_api_port = process.env.cc_api_port
 var cron = require('node-cron');
 
 cron.schedule('*/10 * * * * *', function(){
-   var json = JSON.parse({"secondary_cur":["BTC","ETH","XRP","BSV","BCH","OMG"]})
+   var json = JSON.parse('{"secondary_cur":["BTC","ETH","XRP","BSV","BCH","OMG"]}')
    console.log(json)
    postToCC(json)
    console.log("done")
